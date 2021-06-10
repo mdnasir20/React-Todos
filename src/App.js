5b9506d4197dac97361f309ebae2ca1631f4e370
mdnasir20 <mdnasir.fi@gmail.com>
2021-06-10 12:25:28 +0300

    header title change

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Todos</h1>
+        <h1>My Todos</h1>
       </header>
       <Form
         inputText={inputText}
